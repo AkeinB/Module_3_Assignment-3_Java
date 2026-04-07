56650cbe746d77acb8c43bc6e3c220fcd79a9f79
Gx-a <62352906+Gx-a@users.noreply.github.com>
2026-04-07 15:22:22 -0500

    again

--- src/ConversionLG.java
+++ src/ConversionLG.java
@@ -19,7 +19,6 @@ public class ConversionLG {
 
     public double Converter(double amount, String currency) {
 
-
         return switch (currency) {
             case "USD" -> amount * USD;
             case "CAN" -> amount * CAN;
